194afddd04c8b78f2e1e7dccea81ba6beb8ca0bd
Uhan_33 <jazzcafe12@kangwon.ac.kr>
2024-03-15 18:29:43 +0900

    마지막 수정!

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ npm install
 
 # PM2로 앱 관리
 # 이미 실행 중인 앱이 있으면 삭제
-pm2 delete cicdTest || true
+pm2 delete CI-CD-test || true
 
 # 앱을 PM2로 다시 등록 및 시작
-pm2 start npm --name "cicdTest" -- run start
+pm2 start npm --name "CI-CD-test" -- run start
 
 # PM2 설정 저장 (서버 재시작시 앱 자동 시작을 위함)
 pm2 save
